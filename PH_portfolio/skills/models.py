af0d871eea1214cda41d676c3856680b2c86dd26
Alexander <aleksandr.tailakov@mail.ru>
2023-08-26 22:11:34 +0700

    debug 1

--- PH_portfolio/skills/models.py
+++ PH_portfolio/skills/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+
 class Skill(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
-    photo = models.ImageField(upload_to='skills/images')
+    image = models.ImageField(upload_to='skills/images')
     url = models.URLField(blank=True)
 
     def __str__(self):
