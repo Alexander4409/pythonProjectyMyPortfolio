eae3c98edd6e6f46585efdefe8362fd50b969d62
Alexander <aleksandr.tailakov@mail.ru>
2023-09-04 13:23:52 +0700

    add price

--- PH_portfolio/skills/models.py
+++ PH_portfolio/skills/models.py
@@ -6,6 +6,7 @@ class Skill(models.Model):
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='skills/images')
     url = models.URLField(blank=True)
+    price_per_hour = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
     def __str__(self):
         return self.title
