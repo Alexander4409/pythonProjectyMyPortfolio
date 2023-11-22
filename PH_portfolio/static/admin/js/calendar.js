91ab1895c4484e3bd9937d7e879f2c5d12b9fe2d
Alexander <aleksandr.tailakov@mail.ru>
2023-11-22 14:39:02 +0700

    added assembler for foto sessions data

--- PH_portfolio/static/admin/js/calendar.js
+++ PH_portfolio/static/admin/js/calendar.js
@@ -4,6 +4,7 @@ calendar.js - Calendar functions by Adrian Holovaty
 depends on core.js for utility functions like removeChildren or quickElement
 */
 'use strict';
+
 {
     // CalendarNamespace -- Provides a collection of HTML calendar-related helper functions
     const CalendarNamespace = {
@@ -45,10 +46,10 @@ depends on core.js for utility functions like removeChildren or quickElement
             pgettext('one letter Saturday', 'S')
         ],
         firstDayOfWeek: parseInt(get_format('FIRST_DAY_OF_WEEK')),
-        isLeapYear: function(year) {
-            return (((year % 4) === 0) && ((year % 100) !== 0 ) || ((year % 400) === 0));
+        isLeapYear: function (year) {
+            return (((year % 4) === 0) && ((year % 100) !== 0) || ((year % 400) === 0));
         },
-        getDaysInMonth: function(month, year) {
+        getDaysInMonth: function (month, year) {
             let days;
             if (month === 1 || month === 3 || month === 5 || month === 7 || month === 8 || month === 10 || month === 12) {
                 days = 31;
@@ -64,24 +65,13 @@ depends on core.js for utility functions like removeChildren or quickElement
             }
             return days;
         },
-        draw: function(month, year, div_id, callback, selected) { // month = 1-12, year = 1-9999
+        draw: function (month, year, div_id, callback, selected, bookedDates) {
             const today = new Date();
             const todayDay = today.getDate();
             const todayMonth = today.getMonth() + 1;
             const todayYear = today.getFullYear();
             let todayClass = '';
 
-            // Use UTC functions here because the date field does not contain time
-            // and using the UTC function variants prevent the local time offset
-            // from altering the date, specifically the day field.  For example:
-            //
-            // ```
-            // var x = new Date('2013-10-02');
-            // var day = x.getDate();
-            // ```
-            //
-            // The day variable above will be 1 instead of 2 in, say, US Pacific time
-            // zone.
             let isSelectedMonth = false;
             if (typeof selected !== 'undefined') {
                 isSelectedMonth = (selected.getUTCFullYear() === year && (selected.getUTCMonth() + 1) === month);
@@ -95,7 +85,6 @@ depends on core.js for utility functions like removeChildren or quickElement
             quickElement('caption', calTable, CalendarNamespace.monthsOfYear[month - 1] + ' ' + year);
             const tableBody = quickElement('tbody', calTable);
 
-            // Draw days-of-week header
             let tableRow = quickElement('tr', tableBody);
             for (let i = 0; i < 7; i++) {
                 quickElement('th', tableRow, CalendarNamespace.daysOfWeek[(i + CalendarNamespace.firstDayOfWeek) % 7]);
@@ -106,7 +95,6 @@ depends on core.js for utility functions like removeChildren or quickElement
 
             let nonDayCell;
 
-            // Draw blanks before first of month
             tableRow = quickElement('tr', tableBody);
             for (let i = 0; i < startingPos; i++) {
                 nonDayCell = quickElement('td', tableRow, ' ');
@@ -118,10 +106,10 @@ depends on core.js for utility functions like removeChildren or quickElement
                     e.preventDefault();
                     callback(y, m, this.textContent);
                 }
+
                 return onClick;
             }
 
-            // Draw days of month
             let currentDay = 1;
             for (let i = startingPos; currentDay <= days; i++) {
                 if (i % 7 === 0 && currentDay !== 1) {
@@ -133,7 +121,6 @@ depends on core.js for utility functions like removeChildren or quickElement
                     todayClass = '';
                 }
 
-                // use UTC function; see above for explanation.
                 if (isSelectedMonth && currentDay === selected.getUTCDate()) {
                     if (todayClass !== '') {
                         todayClass += " ";
@@ -145,9 +132,20 @@ depends on core.js for utility functions like removeChildren or quickElement
                 const link = quickElement('a', cell, currentDay, 'href', '#');
                 link.addEventListener('click', calendarMonth(year, month));
                 currentDay++;
+
+                const currentDate = new Date(year, month - 1, currentDay - 1);
+                const formattedDate = currentDate.toISOString().split('T')[0];
+                if (bookedDates.includes(formattedDate)) {
+                    cell.className += ' booked'; // добавляем класс для стилизации
+                }
+
+                // Задаем цвета для выбранных дат
+                if (isSelectedMonth && selected.getUTCDate() === currentDay - 1) {
+                    cell.style.backgroundColor = 'green'; // Задаем цвет фона для выбранной даты
+                    cell.style.color = 'white'; // Задаем цвет текста для выбранной даты
+                }
             }
 
-            // Draw blanks after end of month (optional, but makes for valid code)
             while (tableRow.childNodes.length < 7) {
                 nonDayCell = quickElement('td', tableRow, ' ');
                 nonDayCell.className = "nonday";
@@ -157,13 +155,7 @@ depends on core.js for utility functions like removeChildren or quickElement
         }
     };
 
-    // Calendar -- A calendar instance
-    function Calendar(div_id, callback, selected) {
-        // div_id (string) is the ID of the element in which the calendar will
-        //     be displayed
-        // callback (string) is the name of a JavaScript function that will be
-        //     called with the parameters (year, month, day) when a day in the
-        //     calendar is clicked
+    function Calendar(div_id, callback, selected, bookedDates) {
         this.div_id = div_id;
         this.callback = callback;
         this.today = new Date();
@@ -172,22 +164,19 @@ depends on core.js for utility functions like removeChildren or quickElement
         if (typeof selected !== 'undefined') {
             this.selected = selected;
         }
-    }
-    Calendar.prototype = {
-        drawCurrent: function() {
-            CalendarNamespace.draw(this.currentMonth, this.currentYear, this.div_id, this.callback, this.selected);
-        },
-        drawDate: function(month, year, selected) {
+        this.bookedDates = bookedDates || [];
+        this.drawCurrent = function () {
+            CalendarNamespace.draw(this.currentMonth, this.currentYear, this.div_id, this.callback, this.selected, this.bookedDates);
+        };
+        this.drawDate = function (month, year, selected) {
             this.currentMonth = month;
             this.currentYear = year;
-
-            if(selected) {
+            if (selected) {
                 this.selected = selected;
             }
-
             this.drawCurrent();
-        },
-        drawPreviousMonth: function() {
+        };
+        this.drawPreviousMonth = function () {
             if (this.currentMonth === 1) {
                 this.currentMonth = 12;
                 this.currentYear--;
@@ -196,8 +185,8 @@ depends on core.js for utility functions like removeChildren or quickElement
                 this.currentMonth--;
             }
             this.drawCurrent();
-        },
-        drawNextMonth: function() {
+        };
+        this.drawNextMonth = function () {
             if (this.currentMonth === 12) {
                 this.currentMonth = 1;
                 this.currentYear++;
@@ -206,16 +195,23 @@ depends on core.js for utility functions like removeChildren or quickElement
                 this.currentMonth++;
             }
             this.drawCurrent();
-        },
-        drawPreviousYear: function() {
+        };
+        this.drawPreviousYear = function () {
             this.currentYear--;
             this.drawCurrent();
-        },
-        drawNextYear: function() {
+        };
+        this.drawNextYear = function () {
             this.currentYear++;
             this.drawCurrent();
-        }
-    };
+        };
+    }
     window.Calendar = Calendar;
     window.CalendarNamespace = CalendarNamespace;
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const bookedDates = "{{ booked_dates }}".split(','); // преобразуем строку в массив дат
+    const calendar = new Calendar('calendar', null, null, bookedDates);
+    const selectedDates = ['2023-11-26', '2023-11-29']; // ваши выбранные даты
+    calendar.drawCurrent();
+});
