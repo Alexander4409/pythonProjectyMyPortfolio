370d9badcc9f30ec159b7126ae68e7e3fd930b40
Alexander <aleksandr.tailakov@mail.ru>
2023-11-26 15:20:04 +0700

    added js calendar to blog control panels

--- PH_portfolio/blog/forms.py
+++ PH_portfolio/blog/forms.py
@@ -1,8 +1,15 @@
 from django import forms
+from django.contrib.admin.widgets import AdminDateWidget, AdminTimeWidget
+
 from .models import Blog
 
 
 class BlogForm(forms.ModelForm):
+    # date_input = forms.DateField(widget=AdminDateWidget())
     class Meta:
         model = Blog
         fields = ['title', 'description', 'date']
+        widgets = {
+            'date': AdminDateWidget(attrs={'required': False}),
+            'time': AdminTimeWidget(attrs={'required': False}),
+        }
